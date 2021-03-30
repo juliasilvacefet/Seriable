5425dc6129f1a486f3e98500c04b98c344cb8f77
Julia <felipefernandes180302@gmail.com>
2021-03-30 17:12:04 -0300

    Mudança da conexao para https

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { apiResolver } from "next/dist/next-server/server/api-utils";
 
 const api = axios.create({
-    baseURL:"http://apiseriable.herokuapp.com",
+    baseURL:"https://apiseriable.herokuapp.com",
 
 });
 
